2627c1a71f59f6aa23fb162db325e0590523be1d
vdespeisse <vdespeisse@gmail.com>
2016-10-04 16:48:17 +0200

    debug

--- js/browser-mockup.js
+++ js/browser-mockup.js
@@ -2,14 +2,13 @@ function openTab(event,url) {
   var target = event.currentTarget
   var iframe = target.parentElement.querySelector(".content")
   var tabs = target.parentElement.getElementsByClassName("tab")
-
   iframe.src = url
+  console.log(target)
   for (i in tabs) {
     tabs[i].style["border-bottom-color"] = "#cccccc"
   }
 
   console.log(target)
+
   target.style["border-bottom-color"] = "white"
 }
-
-window.test = 3
