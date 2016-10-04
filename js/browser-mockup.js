101985379c8e6fc3827f09b990bca644b03b1676
vdespeisse <vdespeisse@gmail.com>
2016-10-04 16:43:58 +0200

    browser mockup WIP

--- js/browser-mockup.js
+++ js/browser-mockup.js
@@ -1,6 +1,15 @@
 function openTab(event,url) {
-  alert(url)
+  var target = event.currentTarget
+  var iframe = target.parentElement.querySelector(".content")
+  var tabs = target.parentElement.getElementsByClassName("tab")
 
+  iframe.src = url
+  for (i in tabs) {
+    tabs[i].style["border-bottom-color"] = "#cccccc"
+  }
+
+  console.log(target)
+  target.style["border-bottom-color"] = "white"
 }
 
 window.test = 3
