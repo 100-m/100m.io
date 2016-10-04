6c2c5955d602bf893584c14479d5561e86151b76
Valentin Brajon <vbrajon@gmail.com>
2016-10-04 17:53:01 +0200

    * design

--- js/browser-mockup.js
+++ js/browser-mockup.js
@@ -1,14 +1,10 @@
-function openTab(event,url) {
+function openTab(event, url) {
   var target = event.currentTarget
   var iframe = target.parentElement.querySelector(".content")
   var tabs = target.parentElement.getElementsByClassName("tab")
   iframe.src = url
-  console.log(target)
   for (i in tabs) {
-    tabs[i].style["border-bottom-color"] = "#cccccc"
+    tabs[i].classList = "tab"
   }
-
-  console.log(target)
-
-  target.style["border-bottom-color"] = "white"
+  target.classList = "tab active"
 }
