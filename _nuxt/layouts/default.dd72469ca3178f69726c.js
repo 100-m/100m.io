dda5c4b08b2f8aa502e8d25943e48a8a5c28eae6
Clement Joudet <clement.joudet@gmail.com>
2018-01-11 11:36:41 +0100

    New design

--- _nuxt/layouts/default.dd72469ca3178f69726c.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1],{"7Giu":function(e,n,t){"use strict";var u=function(){var e=this,n=e.$createElement;return(e._self._c||n)("nuxt")},r=[],l={render:u,staticRenderFns:r};n.a=l},jPnS:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var u=t("7Giu"),r=t("q3oS"),l=r(null,u.a,!1,null,null,null);n.default=l.exports}});
-//# sourceMappingURL=default.dd72469ca3178f69726c.js.map
